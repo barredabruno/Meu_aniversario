a4fb88e9014b2c4e035f9a3194b12a0862f2d2aa
barredabruno <112954128+barredabruno@users.noreply.github.com>
2025-01-04 14:02:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js - Código JavaScript para interatividade simples
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Ingressos comprados com sucesso! Aproveite a festa!');
+});
