9b5605197a31fa6aa55edcce76bd2f0d48c2dacb
barredabruno <112954128+barredabruno@users.noreply.github.com>
2025-01-06 07:32:19 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// script.js - Código JavaScript para interatividade simples
-
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Ingressos comprados com sucesso! Aproveite a festa!');
-});
